80a00e683d3052da641c38125b2245018674a013
Sagar <sagarattri52@gmail.com>
2023-01-20 14:19:49 +0530

    created newsapp

---
+++ src/Component/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+    
+  render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://static.india.com/wp-content/uploads/2022/10/Ind-vs-SA-LIVE-Updates.jpg":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href={newsUrl} target="blank" className="btn-btn-sm btn btn-dark">Read More</a>
+             </div>
+        </div> 
+      </div>
+    )
+  }
+}
+
+export default NewsItem
