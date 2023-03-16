6f95c69f8e69cc0241556f3f7c0e342621d9a093
Sagar <sagarattri52@gmail.com>
2023-03-17 00:20:41 +0530

    changes done

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -4,7 +4,7 @@ export class NewsItem extends Component {
 
     
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
@@ -12,6 +12,7 @@ export class NewsItem extends Component {
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                 <a href={newsUrl} target="blank" className="btn-btn-sm btn btn-dark">Read More</a>
              </div>
         </div> 
