afdf0b0fe9ffb3a57cdb926937b433320dd1d67c
Sagar <sagarattri52@gmail.com>
2023-03-17 15:00:02 +0530

    Updates

---
+++ precache-manifest.a2a25445ba17d3c5b179fec6d4aa1c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "696d41e94fc376cfa16d",
+    "url": "/NewsData-App/static/css/main.db6e0868.chunk.css"
+  },
+  {
+    "revision": "696d41e94fc376cfa16d",
+    "url": "/NewsData-App/static/js/main.696d41e9.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/NewsData-App/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "6ecab64cb719ad8686c1",
+    "url": "/NewsData-App/static/js/2.6ecab64c.chunk.js"
+  },
+  {
+    "revision": "6e7488eba0aa928a74ec",
+    "url": "/NewsData-App/static/js/runtime~main.6e7488eb.js"
+  },
+  {
+    "revision": "b3b0cf702ec4d4a30ea1ca4841f5681a",
+    "url": "/NewsData-App/index.html"
+  }
+];
